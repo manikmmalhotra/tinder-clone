50ff50c55c037b22b9d6a81c64d3b79a81d40417
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-11-30 20:12:25 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Header';
+import SwipeButtons from './SwipeButtons';
 import TinderCards from './TinderCards';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
